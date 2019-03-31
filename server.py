5ea901bee14ba241e5569a623da9e796e3798896
nMM456 <nmarkou2000@gmail.com>
2019-03-31 17:21:58 -0400

    Deleted clock VC. has the next game on two voice channels.

--- server.py
+++ server.py
@@ -64,10 +64,7 @@ async def on_member_join(member):
   
 async def background_task():
     await bot.wait_until_ready()
-    time = bot.get_channel(561686318501986314)
     while (not bot.is_closed()):
-        cd = datetime.datetime.now() - datetime.timedelta(hours=4)
-        await time.edit(name=cd.strftime("%I:%M:%S %p"))
         global logBuffer
         global errBuffer
         toChannel=bot.get_channel(497193723621277751)
@@ -82,7 +79,43 @@ async def background_task():
             await toChannel.send(toSend)
         if toSend2 != "":
             await toChannel.send(toSend2)
-        await asyncio.sleep(.9)
+        await asyncio.sleep(1)
+
+async def gameVoice():
+    await bot.wait_until_ready()
+    while(not bot.is_closed()):
+        try:
+            url = 'https://api.overwatchleague.com/live-match'
+            url_get = requests.get(url)
+            data = url_get.json()
+            team1 = data["data"]["liveMatch"]["competitors"][0]["abbreviatedName"]
+            team2 = data["data"]["liveMatch"]["competitors"][1]["abbreviatedName"]
+            score1 = str(data["data"]["liveMatch"]["scores"][0]["value"])
+            score2 = str(data["data"]["liveMatch"]["scores"][1]["value"])
+            await channel1.edit(name=team1+": "+str(score1))
+            await channel2.edit(name=team2+": "+str(score2))
+        except:
+            url = 'https://api.overwatchleague.com/schedule'
+            url_get = requests.get(url)
+            data = url_get.json()
+            current = int(time.time())
+            found=False
+            for x in range(len(data["data"]["stages"])):        
+                for i in range(len(data["data"]["stages"][x]["matches"])):
+                    if data["data"]["stages"][x]["matches"][i]["startDateTS"]/1000 > current:
+                        gametime = data["data"]["stages"][x]["matches"][i]["startDateTS"]/1000
+                        atime = time.strftime('%Y-%m-%d %I:%M:%S', time.localtime(gametime-18000))
+                        team1 = data["data"]["stages"][x]["matches"][i]["competitors"][0]["abbreviatedName"]
+                        team2 = data["data"]["stages"][x]["matches"][i]["competitors"][1]["abbreviatedName"]
+                        if not(channel1.name == team1 and channel2.name == atime):
+                            await channel1.edit(name=team1+" VS. "+team2)
+                            await channel2.edit(name=atime)
+                        found=True
+                        break
+                if found:
+                    break
+            await asyncio.sleep(300)
+
 
 if __name__ == '__main__':
     for extension in startup_extensions:
@@ -94,4 +127,5 @@ if __name__ == '__main__':
     sys.stdout = logBuffer = io.StringIO()
     sys.stderr = errBuffer = io.StringIO()
     bot.loop.create_task(background_task())
+    bot.loop.create_task(gameVoice())
     bot.run(tokens["discord"])
