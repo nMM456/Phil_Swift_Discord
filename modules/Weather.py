dcb6e33e2deb4aed3b8348165a65ac65a7ae86cb
nMM456 <nmarkou2000@gmail.com>
2019-03-31 18:07:55 -0400

    Local weather command for Westborough MA

--- modules/Weather.py
+++ modules/Weather.py
@@ -11,12 +11,28 @@ class Weather(commands.Cog):
     async def alerts(self, ctx):
         """Get Alerts for the Worcester area."""
         url = 'https://api.weather.gov/alerts/active/zone/MAZ012'
+        try:
+            url_get = requests.get(url)
+            data = url_get.json()
+            message="```"+data["features"][0]["properties"]["event"]+"\n"+"\n"
+            message+=data["features"][0]["properties"]["headline"]+"\n"+"\n"
+            message+=data["features"][0]["properties"]["description"]+"```"
+            await ctx.send(message)
+        except:
+            await ctx.send("No alerts.")
+    
+    @commands.command()
+    async def weather(self, ctx):
+        """Returns weather in Westborough, MA"""
+        url = 'https://api.weather.gov/points/42.2695,-71.6161/forecast'
         url_get = requests.get(url)
         data = url_get.json()
-        message="```"+data["features"][0]["properties"]["event"]+"\n"+"\n"
-        message+=data["features"][0]["properties"]["headline"]+"\n"+"\n"
-        message+=data["features"][0]["properties"]["description"]+"```"
-        await ctx.send(message)
+        message="```\n"
+        for i in data["properties"]["periods"]:
+            if i["number"] < 3:
+                message += i["name"]+"\n"
+                message += i["detailedForecast"]+"\n\n"
+        await ctx.send(message+"```")
 
 #Not part of class:
 def setup(bot):
